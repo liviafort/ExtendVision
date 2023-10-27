6a1c599f8de287b1f2191f9c2853fe2e45203aa7
geovana.stefani <geovanastefani2011@gmail.com>
2023-10-26 23:49:59 -0300

    add models (Project and User)

---
+++ models/User.py
@@ -0,0 +1,9 @@
+
+class User:
+    def __init__(self, name, registration, password, titration, gender, birth):
+        self.name = name
+        self.registration = registration
+        self.password = password
+        self.titration = titration
+        self.gender = gender
+        self.birth = birth
