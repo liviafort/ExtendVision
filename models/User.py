6ee85b11f3269d87ea22de2c523de25de899cea2
geovana.stefani <geovanastefani2011@gmail.com>
2023-10-27 00:04:38 -0300

    add getters and setters in entitys

--- models/User.py
+++
@@ -1,9 +0,0 @@
-
-class User:
-    def __init__(self, name, registration, password, titration, gender, birth):
-        self.name = name
-        self.registration = registration
-        self.password = password
-        self.titration = titration
-        self.gender = gender
-        self.birth = birth
