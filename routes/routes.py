0bf788a8385e3d4c3054c8d0b970f80a12572406
Emerson <emersonianbezerra@gmail.com>
2023-11-23 00:34:48 -0300

    profile page creation

--- routes/routes.py
+++ routes/routes.py
@@ -28,3 +28,6 @@ def home_professor():
 def home_student():
     return render_template("home/home_student.html")
 
+@app_routes.route('/student/account/profile')
+def profile():
+    return render_template("account/profile.html")
