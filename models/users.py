4262c1931d6a11f61862601729ca653e1a3c01be
IvyssonUchoa <ivysson2312@gmail.com>
2023-11-23 22:51:05 -0300

    Padrão Singleton adicionado

--- models/users.py
+++ models/users.py
@@ -1,23 +1,26 @@
-from database.supabd import supabase
+#from database.supabd import supabase
+from entitys.SupabaseSingleton import SupabaseSingleton
 from flask import jsonify
 
+supabase_singleton = SupabaseSingleton()
+
 def get_user_by_id(user_id):
-    data = supabase.table("User").select("*").eq("id",user_id).execute()
+    data = supabase_singleton.supabase.table("User").select("*").eq("id",user_id).execute()
     data = data.__dict__
     return data['data'][0]
 
 def get_user_by_email(user_email):
-    data = supabase.table("User").select("*").eq("email",user_email).execute()
+    data = supabase_singleton.supabase.table("User").select("*").eq("email",user_email).execute()
     data = data.__dict__
     return data['data'][0]
 
 def get_users():
-    data = supabase.table("User").select("*").execute()
+    data = supabase_singleton.supabase.table("User").select("*").execute()
     data = data.__dict__
     return data['data']
 
 def create_user(registration, password, name, title, gender, birth, email):
-    data = supabase.table("User").insert({'registration':registration,
+    data = supabase_singleton.supabase.table("User").insert({'registration':registration,
         'password':password,
         'name':name,
         'title':title,
@@ -29,7 +32,7 @@ def create_user(registration, password, name, title, gender, birth, email):
     return data['data'][0]['id']
     
 def update_user(user_id, registration, password, name, title, gender, birth, email):
-    data = supabase.table("User").update({'registration':registration,
+    data = supabase_singleton.supabase.table("User").update({'registration':registration,
         'password':password,
         'name':name,
         'title':title,
@@ -41,7 +44,7 @@ def update_user(user_id, registration, password, name, title, gender, birth, ema
     return data['data'][0]['id']
 
 def delete_user(user_id):
-    data = supabase.table("User").delete().eq("id", user_id).execute()
+    data = supabase_singleton.supabase.table("User").delete().eq("id", user_id).execute()
     data = data.__dict__
     return data['data'][0]
 
